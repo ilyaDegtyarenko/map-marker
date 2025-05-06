f6864a22bf6f0bca2aa714a5e6cb22f0ee090477
Ilya Degtyarenko <is.degtyarenko@gmail.com>
2025-05-06 12:54:55 +0300

    refactor: Move "pinia" setup to separate plugin

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,16 @@ import './assets/styles/css/libs.css'
 import './assets/styles/sass/main.sass'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import pinia from '@/plugins/pinia.ts'
 import vuetify from '@/plugins/vuetify.ts'
 
 const app = createApp(App)
 
-app.use(createPinia())
 app.use(router)
+app.use(pinia)
 app.use(vuetify)
 
 app.mount('#app')
